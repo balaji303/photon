cd91f589c8a10696b16571223e34b62468dc93e2
Balaji <balajivelu303@outlook.com>
2020-09-16 08:13:46 +0530

    Update Blink2.ino

--- Examples/LED/Blink2.ino
+++ Examples/LED/Blink2.ino
@@ -1,1 +1,13 @@
+void setup() {
+pinMode(D7,OUTPUT);
+pinMode(D6,OUTPUT);
+}
 
+void loop() {
+    digitalWrite(D7,LOW);
+    digitalWrite(D6,HIGH);
+    delay(500);
+    digitalWrite(D6,LOW);
+    digitalWrite(D7,HIGH);
+    delay(500);
+}

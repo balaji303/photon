bb1088222e3aac8911ff6fe325dcec1c6654e791
Balaji <balajivelu303@outlook.com>
2020-09-18 07:36:08 +0530

    Update variable.ino

--- Examples/Particle/variable.ino
+++ Examples/Particle/variable.ino
@@ -1,1 +1,32 @@
 
+int status=0;
+void setup() {
+pinMode(D7,OUTPUT);
+pinMode(D6,OUTPUT);
+Particle.function("led",ledtoggle);
+Particle.variable("status",status);
+}
+
+void loop() {
+digitalWrite(D7,HIGH);
+delay(1000);
+digitalWrite(D7,LOW);
+delay(1000);
+status=digitalRead(D6);
+}
+
+
+int ledtoggle(String cmd ){
+    if (cmd=="HIGH"){
+        digitalWrite(D6,1);
+        return 1;
+    }
+    
+    else if(cmd == "LOW"){
+        digitalWrite(D6,0);
+        return 0;
+    }
+    else{
+        return -1;
+    }
+}

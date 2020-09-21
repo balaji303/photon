b22e796c60a0a37dbbefad6339cbe6123afec439
Balaji <balajivelu303@outlook.com>
2020-09-21 07:53:51 +0530

    Update SerialPrint.ino

--- Examples/Serial/SerialPrint.ino
+++ Examples/Serial/SerialPrint.ino
@@ -1,3 +1,13 @@
+/*
+Connect the Photon to your Computer
+Open any serial Terminal APP (TeraTerm)
+Give the Port the photon is connected
+select Baud Rate as 9600
+Enter any value: 1
+It will display as follows
+Photon says it receieved
+49
+*/
 int input=0;
 void setup() {
 Serial.begin(9600);

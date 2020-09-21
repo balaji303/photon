b9aaf8e688c8e271e21effdf944f98c486f53add
Balaji <balajivelu303@outlook.com>
2020-09-21 07:49:09 +0530

    Update SerialPrint.ino

--- Examples/Serial/SerialPrint.ino
+++ Examples/Serial/SerialPrint.ino
@@ -1,1 +1,13 @@
+int input=0;
+void setup() {
+Serial.begin(9600);
+}
 
+void loop() {
+if(Serial.available()>0){
+    input= Serial.read();
+    
+    Serial.println("Photon says it receieved");
+    Serial.println(input);
+}
+}

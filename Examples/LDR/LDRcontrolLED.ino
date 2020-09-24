f0da2f5c15df176fb17344ab7811a7c9314845e5
Balaji <balajivelu303@outlook.com>
2020-09-24 08:14:39 +0530

    Update LDRcontrolLED.ino

--- Examples/LDR/LDRcontrolLED.ino
+++ Examples/LDR/LDRcontrolLED.ino
@@ -1,1 +1,18 @@
+int light=0;
+void setup() {
+pinMode(A0,INPUT);
+pinMode(D7,OUTPUT);
+Particle.variable("light",light);
+}
 
+void loop() {
+light=analogRead(A0);
+if(light<300){
+    digitalWrite(D7,HIGH);
+    delay(500);
+}
+else{
+    digitalWrite(D7,LOW);
+    delay(500);
+}
+}

28486f15028d5d010250b819f36d864d7f6fd887
Balaji <balajimail@zoho.com>
2023-11-22 07:34:17 +0530

    Like, "Baby, baby, baby, oh"

--- Examples/LED/WebBlink.ino
+++ Examples/LED/WebBlink.ino
@@ -1,36 +1,60 @@
-/*
-Program the below code in build.particle.io
-Check the Console in console.particle.io
-In functions section you can see LED function that function enter "HIGH"
-or "LOW" to turn the LED ON and OFF
-as per your requirement
-*/
-void setup() {
-pinMode(D7,OUTPUT);
-pinMode(D6,OUTPUT);
+/**
+ * @file WebBlink.ino
+ * @author @balaji303 (https://github.com/balaji303)
+ * @brief LED blink code using arguments from the webpage
+ * Program the below code in build.particle.io
+ *   Check the Console in console.particle.io
+ *   In functions section you can see LED function that function enter "HIGH"
+ *   or "LOW" to turn the LED ON and OFF
+ *   as per your requirement
+ * @version 1
+ * @date 20-11-2023
+ *
+ * Copyright (c) 2023 @balaji303
+ *
+ */
 
-Particle.function("led",ledtoggle);
+void setup() 
+{
+    pinMode(D7,OUTPUT); // in-build LED
+    pinMode(D6,OUTPUT); // user LED
+    Particle.function("ledaw",ledtoggle);
 }
 
-void loop() {
-digitalWrite(D7,HIGH);
-delay(1000);
-digitalWrite(D7,LOW);
-delay(1000);
+void loop() 
+{
+    digitalWrite(D7,HIGH);
+    delay(100);
+    digitalWrite(D7,LOW);
+    delay(100);
 }
 
-
-int ledtoggle(String cmd ){
-    if (cmd=="HIGH"){
+/**
+ * @brief ledtoggle function is called from console and cmd string is sent from console
+ * 
+ * @param cmd 
+ * @return int 
+ */
+int ledtoggle(String cmd )
+{
+    if (cmd=="0")
+    {
         digitalWrite(D6,HIGH);
+        digitalWrite(D7,HIGH);
+        delay(5000);
         return 1;
     }
     
-    else if(cmd == "LOW"){
+    else if(cmd == "1")
+    {
         digitalWrite(D6,LOW);
+        digitalWrite(D7,LOW);
+        delay(5000);
         return 0;
     }
-    else{
+    
+    else
+    {
         return -1;
     }
 }
--- Examples/Serial/SerialPrint.ino
+++ Examples/Serial/SerialPrint.ino
@@ -1,23 +1,36 @@
 /*
-Connect the Photon to your Computer
-Open any serial Terminal APP (TeraTerm)
-Give the Port the photon is connected
-select Baud Rate as 9600
-Enter any value: 1
-It will display as follows
-Photon says it receieved
-49
+
 */
+/**
+ * @file LDRcontrolLED.ino
+ * @author @balaji303 (https://github.com/balaji303)
+ * @brief Connect the Photon to your Computer
+ *          Open any serial Terminal APP (TeraTerm)
+ *          Give the Port the photon is connected
+ *          select Baud Rate as 9600
+ *          Enter any value: 1
+ *          It will display as follows
+ *          Photon says it receieved
+ *          49
+ * @version 1
+ * @date 20-11-2023
+ *
+ * Copyright (c) 2023 @balaji303
+ *
+ */
 int input=0;
-void setup() {
-Serial.begin(9600);
+void setup()
+{
+    Serial.begin(9600);
 }
 
-void loop() {
-if(Serial.available()>0){
-    input= Serial.read();
+void loop()
+{
+    if(Serial.available()>0)
+    {
+        input= Serial.read();
     
-    Serial.println("Photon says it receieved");
-    Serial.println(input,DEC);
-}
+        Serial.println("Photon says it receieved");
+        Serial.println(input,DEC);
+    }
 }
